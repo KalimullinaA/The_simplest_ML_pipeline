44a2a291544a9e37b56be39b16c06eb7a1d97912
KukartsevaElena <kukarceva_elena@mail.ru>
2023-03-11 21:54:59 +0500

    delete nan

--- create_Dataset.py
+++ create_Dataset.py
@@ -8,8 +8,5 @@ train, test = train_test_split(dataframe, test_size=0.2)
 train['Gender'] = train['Gender'].apply(lambda x:0 if 'male' else 1)
 test['Gender'] = test['Gender'].apply(lambda x:0 if 'male' else 1)
 
-train['Age'] = train['Age'].fillna(train.Age.mean())
-test['Age'] = train['Age'].fillna(test.Age.mean())
-
 train[['Age', 'Gender', 'Education Level', 'Job Title', 'Years of Experience', 'Salary']].to_csv('data_train.csv', index = False)
-test[['Age', 'Gender', 'Education Level', 'Years of Experience','Job Title']].to_csv('data_test.csv', index = False)
+test[['Age', 'Gender', 'Education Level', 'Job Title', 'Years of Experience']].to_csv('data_test.csv', index = False)
