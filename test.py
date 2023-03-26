c0bd5449a49e233ca5d94b359c4a37260b1d7c60
KalimullinaA <kalimullina.alina2009@yandex.ru>
2023-03-26 23:51:19 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,5 @@
-import pandas as pd
 import pickle
-from sklearn.metrics import f1_score
+import pandas as pd
 
-
-LogReg = pickle.load(open('model.pkl', 'rb'))
-X_test = pd.read_csv('test/X_test.csv', delimiter = ',')
-Y_test = pd.read_csv('test/Y_test.csv', delimiter = ',')
-y_preds = LogReg.predict(X_test)
-print(f'f1_score: {f1_score(Y_test, y_preds, average="micro")}')
+load_model = pickle.load(open('model.pkl', 'rb'))
+data_test = pd.read_csv('test/data_test.csv')
